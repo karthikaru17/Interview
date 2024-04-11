49dd082db59cf3dc8a270227634076750d1d0e24
karthikaru17 <karthikaru17@gmail.com>
2024-04-11 15:36:23 -0400

    Newly created Spring Boot Project

---
+++ KarthikCoding/karthik/src/main/java/com/karlib/karthik/KarthikApplication.java
@@ -0,0 +1,13 @@
+package com.karlib.karthik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarthikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarthikApplication.class, args);
+	}
+
+}
---
+++ KarthikCoding/karthik/src/test/java/com/karlib/karthik/KarthikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlib.karthik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarthikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
